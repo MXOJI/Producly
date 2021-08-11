8ec9d95c6336eac27748cb40cf89de7cec6db214
userxoji <khoji97@mail.ru>
2021-08-11 23:00:19 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+$(function () {
+
+    $('.slider').slick({
+
+        arrows: false,
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 1175,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 769,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            }
+        ]
+    });
+
+    $('.menu__btn').on('click', function () {
+        $('.menu__list').toggleClass('menu__list--active')
+    });
+
+
+
+
+});
